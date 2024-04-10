d274a9dc44f1cb8dbfa180908d0bb7e1f304764b
C31ia <cejswh@gmail.com>
2024-04-11 11:30:19 +1200

    Add return link.

--- client/pages/NotFound.tsx
+++ client/pages/NotFound.tsx
@@ -1,9 +1,16 @@
+import { Link } from 'react-router-dom'
+
 function NotFound() {
   return (
     <>
-      <h3>Uh Oh! The page you are looking for is not found</h3>
+      <h3>Uh Oh! The page you are looking for is not found.</h3>
       <img src="../../public/404.png" alt="sad face"></img>
       <h1>404</h1>
+      <p>
+        please
+        <Link to={'/'}> return </Link>
+        to work
+      </p>
     </>
   )
 }
