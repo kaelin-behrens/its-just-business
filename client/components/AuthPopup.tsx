0c03811f615e3648930db620c30a4fa69a8f9f1f
Cae <gemwright@proton.me>
2024-04-11 10:53:07 +1200

    Add password input and handlers

--- client/components/AuthPopup.tsx
+++ client/components/AuthPopup.tsx
@@ -1,11 +1,29 @@
+import { useState } from "react"
 
 function AuthPopup() {
+  const [password, setPassword] = useState('')
+
+  function handleChange(e){
+    setPassword(e.target.value)
+    console.log(password)
+
+  }
+
+  function handleSubmit(e){
+    e.preventDefault()
+    console.log(`Submitted: ${password}`)
+  }
 
 
     return (
-      <>
+      // Temporary inline styling for the purposes of viewing and manually testing components
+      <div style={{border: 'grey 1px solid', backgroundColor: 'beige'}}>
         <p>fake Auth Popup</p>
-      </>
+        <form onSubmit={handleSubmit}>
+          <input type="password" value={password} onChange={handleChange}></input>
+          <button type="submit" style={{border: 'black 1px solid', backgroundColor: 'bisque'}}>Submit</button>
+        </form>
+      </div>
     )
   }
   
