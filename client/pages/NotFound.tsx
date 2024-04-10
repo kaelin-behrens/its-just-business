7efd2ca1bbcea648e386ebb257c691e129bf48ad
C31ia <cejswh@gmail.com>
2024-04-11 10:47:16 +1200

    Add MVP 404 page w/o styling.

--- client/pages/NotFound.tsx
+++ client/pages/NotFound.tsx
@@ -1,12 +1,13 @@
-
 function NotFound() {
-
-
   return (
     <>
-      <p>404 ERROR</p>
+      <h3>Uh Oh! The page you are looking for is not found</h3>
+      <img src="../../public/404.png" alt="sad face"></img>
+      <h1>404</h1>
     </>
   )
 }
+
+//Todo Add styling and remove background of/replace image
 
 export default NotFound
