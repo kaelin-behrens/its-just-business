85e24cdac49dc6d9c0f329e3e233eece3fdc3260
Cae <gemwright@proton.me>
2024-04-11 14:13:54 +1200

    Conditional render CAPTCHA on submit

--- client/components/FormHP.tsx
+++ client/components/FormHP.tsx
@@ -1,10 +1,19 @@
+import { useState } from "react"
 
 function FormHP() {
+  const [displayCaptcha, setDisplayCaptcha] = useState(false)
+
+  function handleSubmit(e){
+    e.preventDefault()
+    setDisplayCaptcha(true)
+  }
+
   const reportText = "Q1 witnessed robust sales growth across multiple segments, underscoring the resilience and adaptability of our sales force. Despite prevailing market headwinds, our strategic initiatives have yielded commendable results, bolstering our market positioning and driving sustained revenue expansion.Aggressive market penetration strategies led to a notable increase in our market share, consolidating our foothold in key territories. By leveraging targeted marketing campaigns and fostering strategic partnerships, we have augmented our brand visibility and expanded our customer base."
 
     return (
       <div style={{border: 'grey 1px solid', backgroundColor: 'lightgrey'}}>
-        <form>
+        {!displayCaptcha && 
+        <form onSubmit={handleSubmit}>
           <input defaultValue={"Executive Summary"}></input>
           <textarea defaultValue={reportText} rows={6} ></textarea>
           <label htmlFor="quarter">Financial quarter:</label>
@@ -15,7 +24,8 @@ function FormHP() {
             <option value="Q4">Q4</option>
           </select>
           <button type="submit" style={{border: 'black 1px solid', backgroundColor: 'bisque'}}>Submit</button>
-        </form>
+        </form>}
+        {displayCaptcha && <p>CAPTCHA</p>}
       </div>
     )
   }
