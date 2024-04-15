7d54b151979263bf6ba7a83ad086212ce8a92dc6
C31ia <cejswh@gmail.com>
2024-04-15 14:30:59 +1200

    add y-title with onclick function.

--- client/components/DragAndDrop/Graph.tsx
+++ client/components/DragAndDrop/Graph.tsx
@@ -3,10 +3,13 @@ import { SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable'
 import { Bar } from './Bar'
 
 import './Graph.css'
+import { useState } from 'react'
 
 export const Graph = ({ bars }) => {
-  const [duplicate, setDuplicate] = useState('duplicate')
-  const handleClick = () => {}
+  const [duplicate, setDuplicate] = useState(['Profits'])
+  const handleClick = () => {
+    setDuplicate([...duplicate, 'Profits'])
+  }
   return (
     <div className="graph">
       <SortableContext items={bars} strategy={verticalListSortingStrategy}>
@@ -14,9 +17,11 @@ export const Graph = ({ bars }) => {
           <Bar key={bar.id} id={bar.id} title={bar.title} />
         ))}
       </SortableContext>
-      <p className="yTitle" onClick={handleClick()}>
-        Profits
-      </p>
+      {duplicate.map((title) => (
+        <button className="yTitle" onClick={handleClick} key={title}>
+          {title}
+        </button>
+      ))}
     </div>
   )
 }
