5818b4c640885f448c517e76761eec5af4d4ca15
Cae <gemwright@proton.me>
2024-04-17 10:15:04 +1200

    refactor

--- client/components/AltCodeBreaker.tsx
+++ client/components/AltCodeBreaker.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 
-
 function shuffle(array) {
     let currentIndex = array.length;
     while (currentIndex != 0) {
@@ -13,11 +12,15 @@ function shuffle(array) {
   }
 
 
-
 function AltCodeBreaker(){
-    const [sequence, setSequence] = useState([])
+    const [sequence, setSequence] = useState([0])
     const numbers = Array.from({ length: 10 }, (_, index) => index)
-    const [selectedNumbers, setSelectedNumbers] = useState([1, 2])
+    const [selectedNumbers, setSelectedNumbers]  = useState([1])
+    const [win, setWin] = useState(false)
+    const [roundOver, setRoundOver]  = useState(false)
+    const [red, setRed] = useState([])
+    const [yellow, setYellow] = useState([])
+    const [green, setGreen] = useState([])
 
     useEffect(() => {
         initializeGame() 
@@ -27,6 +30,9 @@ function AltCodeBreaker(){
     function initializeGame(){
         const shuffledNumbers = shuffle(numbers)
         setSequence(shuffledNumbers.slice(0, 4))
+        console.log(shuffledNumbers.slice(0, 4))
+        setSelectedNumbers([])
+        
     }
 
     function handleNumberClick(e){
@@ -34,23 +40,62 @@ function AltCodeBreaker(){
         console.log(updatedSelect)
         setSelectedNumbers(updatedSelect) 
         if(selectedNumbers.length == 3){
-            checkSequence()
+            setRoundOver(true)
+        }
+    }
+
+    function checkSequence(selection : number[]) {
+        console.log(sequence.toString())
+        console.log(selection.toString())
+        if(sequence.toString() == selection.toString()){
+            return true
+        } else {
+            return false
+        }
+    }
+
+    function getNumberColor() {
+        const newRed = [... red]
+        const newYellow = [... yellow]
+        const newGreen = [... green]
+        selectedNumbers.map((item, index) => {
+            if(item === sequence[index]){
+                if(newYellow.includes(item)){
+                    newYellow.splice(index)
+                    newGreen.push(item)
+                }
+                else {newGreen.push(item)}
+                console.log('correct')
+            } else if (sequence.includes(item)){
+                if(newGreen.includes(item)){
+                    newGreen.splice(index)
+                    newYellow.push(item)
+                }
+                else {newYellow.push(item)}
+                console.log('correct')
+            }
+            else {newRed.push(item)
+            }
+        })
+        setGreen(newGreen) 
+        setYellow(newYellow) 
+        setRed(newRed) 
+        }
+
+    if(roundOver){
+        if(checkSequence(selectedNumbers)){
+            setRoundOver(false)
+        } else{
+            getNumberColor()
+            setRoundOver(false)
             setSelectedNumbers([])
         }
     }
 
-    function checkSequence() {
-        console.log("checking sequence")
-
-    }
-
-    function getNumberColor() {
-        console.log(getNumberColor)
-      }
-
     return <>
-    {numbers.map((item, index) => <button key={index} value={item} onClick={handleNumberClick}>{item}</button>)}
+    {numbers.map((item, index) => <button key={index} value={item} className={red.includes(item) ? "codered" : yellow.includes(item) ? "codeyellow" : green.includes(item) ? "codegreen" : "nope"} onClick={handleNumberClick}>{item}</button>)}
     <p>{selectedNumbers.map((item, index) => <span key={index}> {item} </span>)}</p>
+    <p>{red.map((item, index) => <span key={index}> {item} </span>)}</p>
     </>
 }
 
