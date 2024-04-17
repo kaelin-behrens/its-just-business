c1424daa0cd37eddcaaaa3dcbf7e507304d6c921
Cae <155507582+cae-stone@users.noreply.github.com>
2024-04-17 17:03:09 +1200

    Update Home.tsx

--- client/pages/Home.tsx
+++ client/pages/Home.tsx
@@ -52,68 +52,70 @@ function Home() {
   }, [])
 
   return (
-    <>
-      {surveyTime && !complete && (
-        <Survey current={complete} new={setComplete} />
-      )}
-      {showKPI && <KPIPopup />}
-      <div className="userbanner">
-        <p className="usergreeting">
-          Welcome back User93748GB57, work hours have commenced.
-        </p>
-
-        <p className="timertext">
-          This report is due in{' '}
-          <Timer
-            currentSurveyState={surveyTime}
-            newSurveyState={setSurveyTime}
-          />
-        </p>
-        {/*//TODO replace with dynamic time */}
-      </div>
-      <div>
-        <img
-          className="businesslogo"
-          src="../../public/logo.svg"
-          alt="logo"
-        ></img>
-      </div>
+  <>
+    {surveyTime && !complete && (
+      <Survey current={complete} new={setComplete} />
+    )}
+    {showKPI && <KPIPopup />}
+    <div className="userbanner">
+      <p className="usergreeting">
+        Welcome back User93748GB57, work hours have commenced.
+      </p>
+      <p className="timertext">
+        This report is due in{' '}
+        <Timer
+          currentSurveyState={surveyTime}
+          newSurveyState={setSurveyTime}
+        />
+      </p>
+      {/*//TODO replace with dynamic time */}
+    </div>
+    <div>
+      <img
+        className="businesslogo"
+        src="../../public/logo.svg"
+        alt="logo"
+      ></img>
+    </div>
+    <div className="wrapper">
       <div className="header">
-        <div>
-          <h1 className="title">Important Business Report</h1>
-          <p className="blandtext">
-            In todays dynamic marketplace, companies must leverage synergistic
-            strategies to maximize their competitive advantage. It is imperative
-            to think outside the box and cultivate a robust ecosystem of
-            innovation, fostering a culture of disruption and agility. In the
-            past quater, we have laid off 1,5243 staff. By aligning key
-            performance indicators with overarching business objectives,
-            organizations can optimize operational efficiencies and drive
-            sustainable growth. Our profitprofits have increased by 179%.
-            Leveraging cutting-edge technologies such as artificial intelligence
-            and blockchain, businesses can streamline processes, enhance
-            customer experiences, and stay ahead of the curve in this
-            hyperconnected landscape.
-          </p>
-
-          {/* <p className="blandtext">
-            Moreover, proactive stakeholder engagement is paramount in
-            navigating the complexities of global markets. By conducting
-            comprehensive market analyses and harnessing actionable insights,
-            enterprises can capitalize on emerging trends and capitalize on
-            untapped opportunities. It is essential to foster cross-functional
-            collaboration and nurture strategic partnerships to fuel organic
-            expansion and fortify market positioning. Through iterative
-            experimentation and iterative optimization, businesses can
-            continuously refine their value propositions and adapt to evolving
-            consumer demands, ensuring long-term viability and resilience in an
-            ever-evolving business landscape.
-          </p> */}
-        </div>
-      </div>
-      <div className="grid-container">
-        <div className="codebreaker grid-item">
-          <CodeBreaker clues={clues} />
+        <div className="row">
+          <div>
+            <h1 className="title">Important Business Report</h1>
+            <p className="blandtext">
+              In todays dynamic marketplace, companies must leverage
+              synergistic strategies to maximize their competitive advantage.
+              It is imperative to think outside the box and cultivate a robust
+              ecosystem of innovation, fostering a culture of disruption and
+              agility. In the past quater, we have laid off 1,5243 staff. By
+              aligning key performance indicators with overarching business
+              objectives, organizations can optimize operational efficiencies
+              and drive sustainable growth. Our profitprofits have increased
+              by 179%. Leveraging cutting-edge technologies such as artificial
+              intelligence and blockchain, businesses can streamline
+              processes, enhance customer experiences, and stay ahead of the
+              curve in this hyperconnected landscape.
+            </p>
+            <p className="blandtext">
+              Moreover, proactive stakeholder engagement is paramount in
+              navigating the complexities of global markets. By conducting
+              comprehensive market analyses and harnessing actionable
+              insights, enterprises can capitalize on emerging trends and
+              capitalize on untapped opportunities. It is essential to foster
+              cross-functional collaboration and nurture strategic
+              partnerships to fuel organic expansion and fortify market
+              positioning. Through iterative experimentation and iterative
+              optimization, businesses can continuously refine their value
+              propositions and adapt to evolving consumer demands, ensuring
+              long-term viability and resilience in an ever-evolving business
+              landscape.
+            </p>
+          </div>
+          <div className="grid-container">
+            <div className="codebreaker grid-item">
+              <CodeBreaker clues={clues} />
+            </div>
+          </div>
         </div>
         <div className="captcha grid-item">
           <FormHP clues={clues} />
@@ -130,10 +132,19 @@ function Home() {
           Submit
         </button>
       </div>
-      {showPopUp && <AuthPopup answer={answer} />}
-      <ChatBotPopup />
-    </>
-  )
+    <div className="finalbuttoncontainer">
+      <button className="finalbutton" onClick={handleSubmit}>
+        Submit
+      </button>
+    </div>
+    </div>
+    {showPopUp && <AuthPopup answer={answer} />}
+    <ChatBotPopup />
+  </>)
 }
+  
+    
+  
+
 
 export default Home
