26d66c580624c146237f518a8356d367b2e96087
zak khan <zakariyyakhan87@gmail.com>
2024-04-17 15:59:49 +1200

    added css

--- client/pages/NotFound.tsx
+++ client/pages/NotFound.tsx
@@ -1,17 +1,19 @@
 import { Link } from 'react-router-dom'
-
+import './../styles/not-found.css'
 function NotFound() {
   return (
-    <>
-      <h3>Uh Oh! The page you are looking for is not found.</h3>
-      <img src="../../public/404.png" alt="sad face"></img>
-      <h1>404</h1>
-      <p>
-        please
-        <Link to={'/'}> return </Link>
-        to work
-      </p>
-    </>
+    <div className="not-found">
+      <body>
+        <h1>404</h1>
+        <p>PAGE NOT FOUND</p>
+        <Link to="/">
+          <img src="../../public/404.png" alt="Back to home" />
+        </Link>
+        <p>
+          Please <Link to="/">return</Link> to work
+        </p>
+      </body>
+    </div>
   )
 }
 
