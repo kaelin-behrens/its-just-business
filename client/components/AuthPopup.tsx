e5c170a11805d1b61e6df08c987aa14ed1368b4e
C31ia <cejswh@gmail.com>
2024-04-17 17:57:06 +1200

    add auth popup.

--- client/components/AuthPopup.tsx
+++ client/components/AuthPopup.tsx
@@ -2,9 +2,7 @@ import { useState } from 'react'
 import AuthVerification from './AuthVerification'
 import { useNavigate } from 'react-router-dom'
 
-
 function AuthPopup(props) {
-
   const [password, setPassword] = useState('')
   const answer = props.answer
   const [goToVerify, setGoToVerify] = useState(false)
@@ -25,19 +23,46 @@ function AuthPopup(props) {
     }
   }
 
-  if(nav){
+  if (nav) {
     navigate('/404')
+  }
+  const [isVisible, setIsVisible] = useState(true)
+
+  const handleClose = () => {
+    setIsVisible(false)
   }
 
   return (
+    <>
+      {isVisible && (
+        <>
+          <div className="survey-cover"></div>
 
-    <div>
-      {!goToVerify && <form onSubmit={handleSubmit}>
-        <input type="password" value={password} onChange={handleChange}></input>
-        <button type="submit">Submit</button>
-      </form> }
-      {goToVerify && <AuthVerification currentNavState={nav} newNavState={setNav}/>}
-    </div>
+          <div className="survey-popup">
+            <div>
+              <p>
+                Thank you for trying to submit your work! Our new cybersecurity
+                measures mean we need you to enter your password:{' '}
+              </p>
+              <button onClick={handleClose}>close</button>
+              {!goToVerify && (
+                <form onSubmit={handleSubmit}>
+                  <input
+                    type="password"
+                    value={password}
+                    onChange={handleChange}
+                  ></input>
+                  <button type="submit">Submit</button>
+                </form>
+              )}
+              {goToVerify && (
+                <AuthVerification currentNavState={nav} newNavState={setNav} />
+              )}
+            </div>
+          </div>
+        </>
+      )}
+    </>
   )
 }
 
